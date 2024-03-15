673b78044ecc4fef4a10ec72296e9050a651ae04
Shivam Pandey <shivamkumarpandey3971@gmail.com>
2024-03-15 20:54:03 +0530

    changes

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./Main.css";
+import { assets } from "../../assets/assets";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>Hello,Dev</span>
+          </p>
+          <p>How can i help you today?</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>suggest beautiful places to see on an upcoming road trip</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Brainstorm team bonding activities for our work retreat</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Briefly summarize this concept:urbon planning</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Improve the readability of the following code</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+        </div>
+
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text" placeholder="Enter a Prompt here" />
+            <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img src={assets.send_icon} alt="" />
+            </div>
+          </div>
+          <p className="bottom-info">
+            Gemini may display inaccurate info,including about people ,so
+            double-check its responses before acting upon them.Powered by Gemini
+            AI
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,40 +1,49 @@
-import React from 'react'
-import './Sidebar.css';
-import  {assets} from '../../assets/assets'
+import React, { useState } from "react";
+import "./Sidebar.css";
+import { assets } from "../../assets/assets";
 
 const Sidebar = () => {
-  return (
-    <div className='sidebar'>
-        <div className="top">
-           <img className='menu' src={assets.menu_icon} alt="" />
-           <div className="new-chat">
-            <img src={assets.plus_icon} alt="" />
-            <p>New Chat</p>
-           </div>
-           <div className="recent">
-            <p className="recent-title">Recent</p>
-                <div className="recent-entry">
-                  <img src={assets.message_icon} alt="" />
-                  <p>What is React...</p>
-                </div>
-           </div>
-        </div>
-        <div className="bottom">
-          <div className="bottom-icon recent-entry">
-            <img src={assets.question_icon} alt="" />
-            <p>Help</p>
-          </div>
-          <div className="bottom-icon recent-entry">
-            <img src={assets.history_icon} alt="" />
-            <p>Activity</p>
-          </div>
-          <div className="bottom-icon recent-entry">
-            <img src={assets.setting_icon} alt="" />
-            <p>Settings</p>
-          </div>
-        </div>
-    </div>
-  )
-}
+  const [extended, setExtended] = useState(false);
 
-export default Sidebar
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <img
+          onClick={() => setExtended((prev) => !prev)}
+          className="menu"
+          src={assets.menu_icon}
+          alt=""
+        />
+        <div className="new-chat">
+          <img src={assets.plus_icon} alt="" />
+          {extended ? <p>New Chat</p> : null}
+        </div>
+        {extended ? (
+          <div className="recent">
+            <p className="recent-title">Recent</p>
+            <div className="recent-entry">
+              <img src={assets.message_icon} alt="" />
+              <p>What is React...</p>
+            </div>
+          </div>
+        ) : null}
+      </div>
+      <div className="bottom">
+        <div className="bottom-icon recent-entry">
+          <img src={assets.question_icon} alt="" />
+          {extended ? <p>Help</p> : null}
+        </div>
+        <div className="bottom-icon recent-entry">
+          <img src={assets.history_icon} alt="" />
+          {extended ? <p>Activity</p> : null}
+        </div>
+        <div className="bottom-icon recent-entry">
+          <img src={assets.setting_icon} alt="" />
+          {extended ? <p>Settings</p> : null}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
